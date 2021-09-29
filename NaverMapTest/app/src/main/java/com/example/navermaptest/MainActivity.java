aa7d0bb5a896f715a05488fa02bdccead1d34114
laewonJeong <laewonjeong@kangwon.ac.kr>
2021-09-29 22:54:43 +0900

    네이버 맵 위도 경도 쓰레드로 하는거 구현

--- NaverMapTest/app/src/main/java/com/example/navermaptest/MainActivity.java
+++ NaverMapTest/app/src/main/java/com/example/navermaptest/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -17,8 +18,14 @@ import android.view.WindowManager;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,11 +34,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -45,17 +47,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void button1(View view) {
-        Location a = addToPoint(this);
-        Log.d("test",a.getLatitude() +" " + a.getLongitude());
+        String str = "강원도 춘천시 서부대성로 245 (효자동)";
+        new Thread(){
+            @Override
+            public void run(){
+                Location a = addToPoint(str);
+                Log.d("test",a.getLatitude() +" " + a.getLongitude());
+            }
+        }.start();
     }
 
-    public static Location addToPoint(Context context){
+    public Location addToPoint(String str){
         Location location = new Location("");
-        Geocoder geocoder = new Geocoder(context);
+        Geocoder geocoder = new Geocoder(this);
         List<Address> addresses = null;
 
         try{
-            addresses = geocoder.getFromLocationName("강원도 춘천시 서부대성로 245 (효자동)",3);
+            addresses = geocoder.getFromLocationName(str,3);
         } catch(IOException e){
             e.printStackTrace();
         }
@@ -68,4 +76,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return location;
     }
+
+
 }
