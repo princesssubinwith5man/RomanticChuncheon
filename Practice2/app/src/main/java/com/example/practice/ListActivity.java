6fd48b8177531577659a0889590f8fc0ab09ee50
YisakH <drm05003@naver.com>
2021-11-03 23:54:49 +0900

    db 스키마 수정했습니당

--- Practice2/app/src/main/java/com/example/practice/ListActivity.java
+++ Practice2/app/src/main/java/com/example/practice/ListActivity.java
@@ -34,7 +34,7 @@ import java.util.Queue;
 
 import static com.example.practice.R.layout.listview_custom;
 public class ListActivity extends AppCompatActivity {
-    ArrayList<HashMap<String,String>> list = new ArrayList<HashMap<String, String>>();
+    ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
     ArrayList<HashMap<String, Shop>> shopList = new ArrayList<>();
     DatabaseReference mDatabase;
     ListView listview;
@@ -43,6 +43,7 @@ public class ListActivity extends AppCompatActivity {
     String sector;
     String temp;
     static int cnt = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,55 +59,54 @@ public class ListActivity extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.list);
         GetData();
     }
-    public void GetData(){
+
+    public void GetData() {
         ListViewAdapter adapter = new ListViewAdapter();
-        for(int i = 0;i<=40;i++) {
-            String dong = Integer.toString(i);
-            HashMap<String, Shop> shopItem = new HashMap<>();
+        HashMap<String, Shop> shopItem = new HashMap<>();
 
-            //mDatabase.child("춘천시").child(dong).orderByValue().equalTo(temp, "sector");
-            mDatabase.child("춘천시").child(dong).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DataSnapshot> task) {
-                    DataSnapshot dataSnapshot = (DataSnapshot) task.getResult();
-                    for (DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
-                        //HashMap<String,String> item = new HashMap<String, String>();
+        //mDatabase.child("춘천시").child(dong).orderByValue().equalTo(temp, "sector");
+        mDatabase.child("shop").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                DataSnapshot dataSnapshot = (DataSnapshot) task.getResult();
+                for (DataSnapshot fileSnapshot : dataSnapshot.getChildren()) {
+                    //HashMap<String,String> item = new HashMap<String, String>();
 
-                        Shop shop = fileSnapshot.getValue(Shop.class);
-                        shopItem.put(shop.name, shop);
-                        address = fileSnapshot.child("address").getValue(String.class);
-                        name = fileSnapshot.child("name").getValue(String.class);
-                        sector = fileSnapshot.child("sector").getValue(String.class);
-                        Log.i("TAG: value is ", name + sector + " : " + temp);
-                        if(name==null || name.isEmpty())continue;
-                        if (sector.equals(temp)) {
-                            Log.i("TAG: value is ", name + " : " + address+", "+sector);
-                            adapter.addItem(0,name,"0",address);
-                        }
-                        cnt++;
-                        //if(cnt > 20400)
-                        //    break;
-                        String a = Integer.toString(cnt);
-                        Log.i("TAG: Total Count ", a);
+                    Shop shop = fileSnapshot.getValue(Shop.class);
+                    shopItem.put(shop.name, shop);
+                    address = fileSnapshot.child("address").getValue(String.class);
+                    name = fileSnapshot.child("name").getValue(String.class);
+                    sector = fileSnapshot.child("sector").getValue(String.class);
+                    Log.i("TAG: value is ", name + sector + " : " + temp);
+                    if (name == null || name.isEmpty()) continue;
+                    if (sector.equals(temp)) {
+                        Log.i("TAG: value is ", name + " : " + address + ", " + sector);
+                        adapter.addItem(0, name, "0", address);
                     }
-                    shopList.add(shopItem);
-                    listview.setAdapter(adapter);
-                    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            ListViewItem listViewItem = adapter.listViewItemList.get(i);
-                            String centerName = listViewItem.getCenterNameStr();
-                            String address = listViewItem.getAddressStr();
-                            //Toast.makeText(getApplicationContext(), "위도 : " + centerName, Toast.LENGTH_LONG).show();
-                            //Log.i("TAG: value is ", centerName + " : " + address);
-                            Intent intent = new Intent(ListActivity.this, InformationActivity.class);
-                            intent.putExtra("centername", centerName);
-                            intent.putExtra("add", address);
-                            startActivity(intent);
-                        }
-                    });
+                    cnt++;
+                    //if(cnt > 20400)
+                    //    break;
+                    String a = Integer.toString(cnt);
+                    Log.i("TAG: Total Count ", a);
                 }
-            });
-        }
+                shopList.add(shopItem);
+                listview.setAdapter(adapter);
+                listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        ListViewItem listViewItem = adapter.listViewItemList.get(i);
+                        String centerName = listViewItem.getCenterNameStr();
+                        String address = listViewItem.getAddressStr();
+                        //Toast.makeText(getApplicationContext(), "위도 : " + centerName, Toast.LENGTH_LONG).show();
+                        //Log.i("TAG: value is ", centerName + " : " + address);
+                        Intent intent = new Intent(ListActivity.this, InformationActivity.class);
+                        intent.putExtra("centername", centerName);
+                        intent.putExtra("add", address);
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
     }
+
 }
