24fe192b4e704e8a4f9bb947cfb4d61feac1997f
YisakH <drm05003@naver.com>
2021-11-04 15:28:09 +0900

    계정 관리 작업중,,,

--- Practice2/app/src/main/java/com/example/practice/InformationActivity.java
+++ Practice2/app/src/main/java/com/example/practice/InformationActivity.java
@@ -8,10 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class InformationActivity extends AppCompatActivity {
@@ -33,9 +36,9 @@ public class InformationActivity extends AppCompatActivity {
         address = intent.getExtras().getString("add");
         like = Integer.parseInt(intent.getExtras().getString("like"));
         key = intent.getExtras().getString("key");
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("shop").child(key);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mDatabase.child("like").addValueEventListener(new ValueEventListener() {
+        mDatabase.child("shop").child(key).child("like").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 likeText.setText(snapshot.getValue().toString());
@@ -55,6 +58,12 @@ public class InformationActivity extends AppCompatActivity {
     public void like(View view) {
         final TextView li = (TextView)findViewById(R.id.likkk);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        Query query = mDatabase.child("like").child(key).child("wholike").equalTo(user.getUid());
+
+
+
+        if(mDatabase.child("like").child(key).child("wholike").get().getResult().getValue().toString())
 
         like++;
         mDatabase.child("like").setValue(like);
--- Practice2/app/src/main/java/com/example/practice/SignupActivity.java
+++ Practice2/app/src/main/java/com/example/practice/SignupActivity.java
@@ -13,6 +13,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -36,11 +39,12 @@ public class SignupActivity extends AppCompatActivity {
         buttonJoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")) {
-                    // 이메일과 비밀번호가 공백이 아닌 경우
+                if (!editTextEmail.getText().toString().equals("") && !editTextPassword.getText().toString().equals("")
+                        && !editTextName.getText().toString().equals("")) {
+                    // 이메일과 비밀번호와 이름이 공백이 아닌 경우
                     createUser(editTextEmail.getText().toString(), editTextPassword.getText().toString(), editTextName.getText().toString());
                 } else {
-                    // 이메일과 비밀번호가 공백인 경우
+                    // 이메일이나 비밀번호나 이름이 공백인 경우
                     Toast.makeText(SignupActivity.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
                     Log.d("asdfsadf", "onClick: 계정과 비밀번호를 입력하세요");
                 }
@@ -54,6 +58,11 @@ public class SignupActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // 회원가입 성공시
+
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            DatabaseReference database = FirebaseDatabase.getInstance().getReference("user").child(user.getUid());
+                            database.child("name").setValue(name);
+
                             Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
                             Log.d("asdfsadf", "onClick: 회원가입 성공");
                             finish();
